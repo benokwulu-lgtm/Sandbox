1dd6d7cfd8e20c232b865a600fc6ef6d556ad554
benokwulu-lgtm <benokwulu@gmail.com>
2026-08-06 11:09:59 +0100

    feat: initialize week-3 sandbox and create console trainee logger

---
+++ app.js
@@ -0,0 +1,22 @@
+// Variables
+
+const traineeName = "Benjamin";
+const traineeAge = 25;
+const isHired = true;
+const monthlySalary = 350000;
+
+let specialty;
+
+specialty = "Full Stack Developer";
+
+// Template Literal
+
+const summary = `
+Trainee ${traineeName} is ${traineeAge} years old.
+Specialty: ${specialty}.
+Hired: ${isHired}.
+`;
+
+// Print to the console
+
+console.log(summary);
